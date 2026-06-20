183c3995083cd70942747d97d56650cea90b8773
vasiekig-droid <vasiekig@gmail.com>
2026-06-20 15:05:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "neurocourse-tracker-v34";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
-self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
-self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request))); });
+const CACHE_NAME = 'neurocourse-tracker-v35';
+const ASSETS = ['./','./index.html','./manifest.json','./sw.js'];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
