ae41e9dfbcb3e748fbdf9ca54f6604c7f592085c
vasiekig-droid <vasiekig@gmail.com>
2026-06-21 17:09:00 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
-НейроКурс Трекер 3.5
-
-Стабильная исправленная версия. Загрузите index.html, manifest.json, sw.js, README.txt в GitHub и откройте сайт с ?v=35.
+const CACHE_NAME='neurocourse-tracker-v45';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = 'neurocourse-tracker-v35';
-const ASSETS = ['./','./index.html','./manifest.json','./sw.js'];
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
+{
+  "name": "НейроКурс Трекер v4.5",
+  "short_name": "НейроКурс",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme_color": "#7c3aed",
+  "description": "Трекер курса v4.5 с хвостами, GitHub Sync и дневным светофором"
+}
